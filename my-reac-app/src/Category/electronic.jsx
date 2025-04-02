1c5bdf5d6ebcd7da119df21c92721883015be31a
Siddikur Rahman <siddikur.dev@gmail.com>
2025-04-02 15:05:18 +0600

    clear history and single history added

--- my-reac-app/src/Category/electronic.jsx
+++ my-reac-app/src/Category/electronic.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Electronic = ({ electronicItem }) => {
-  const { title, image, price } = electronicItem;
+const Electronic = ({ electronicItem, handleGetElectronic }) => {
+  const [addCart, setAddCart] = useState(false);
+  const addToCart = () => {
+    setAddCart(true);
+    handleGetElectronic(electronicItem);
+  };
   return (
-    <div className="grid  grid-cols-3 col-span-3 gap-5">
-      <p>Lorem ipsum dolor sit amet.</p>
+    <div>
+      {/* <!-- Product Grid --> */}
+      <div>
+        <div className="bg-white p-4 rounded-lg shadow-md" v-for="i in 6">
+          <img
+            className="object-cover h-28 justify-center mx-auto "
+            src={electronicItem.image}
+            alt=""
+          />
+          <h2 className="mt-4 text-lg font-semibold">
+            Title: {electronicItem.title}
+          </h2>
+          <p className="text-gray-600">Price: {electronicItem.price}k</p>
+          <div className="gap-4 flex">
+            <button className="mt-2 px-4 py-2 border border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition cursor-pointer">
+              View Details
+            </button>
+            <button
+              onClick={addToCart}
+              className="mt-2 px-4 py-2 border border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition cursor-pointer"
+              disabled={addCart}>
+              {addCart ? "Cart Added" : "Add To Cart"}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- my-reac-app/src/Category/electronics.jsx
+++ my-reac-app/src/Category/electronics.jsx
@@ -1,22 +1,76 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Electronic from "./electronic";
 
 const Electronics = ({ fetchPromise }) => {
   const fetchPromiseUse = use(fetchPromise);
-  // console.log(fetchPromiseUse);
+  console.log(fetchPromiseUse);
+  const [getElectronic, setGetElectronic] = useState([]);
+  const handleGetElectronic = (electronicItem) => {
+    const newGetElectronic = [...getElectronic, electronicItem];
+    setGetElectronic(newGetElectronic);
+  };
+  // history celar
+  const handleClearElectronic = () => {
+    setGetElectronic([]);
+  };
+  //
+  const handleRemoveElectronic = (id) => {
+    const updatedElectronics = getElectronic.filter((item) => item.id !== id);
+    setGetElectronic(updatedElectronics);
+  };
 
   return (
     <div>
       <h3 className="mx-auto p-10 font-bold text-center">
         All Electronic Item: {fetchPromiseUse.length}
       </h3>
-      <div className="grid grid-cols-4 gap-5">
-        <div className="grid grid-cols-1 col-span-1">
-          <h3>HIstiry</h3>
+
+      <div className="flex flex-col md:flex-row  container mx-auto justify-center gap-5">
+        {/* Aside */}
+        <div className=" bg-[#ddd]  grid-cols-1 w-1/3 p-4 rounded-lg shadow-md">
+          <h3 className="font-bold  hover:bg-purple-500 hover:text-white border  border-purple-500 text-purple-500 p-2 rounded-lg">
+            {" "}
+            Cart History: {getElectronic.length}
+          </h3>
+          <button
+            className="mt-2 px-4 py-2 border border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition cursor-pointer mb-2"
+            onClick={handleClearElectronic}
+          >
+            Clear History
+          </button>
+          {/* Clear History Cart */}
+
+          {getElectronic.map((electro) => (
+            <div className="border-2 border-indigo-600 rounded-md p-2  mb-4">
+              <img className="w-12" src={electro.image} alt="" />
+              <h1>
+                Rating:{" "}
+                <div class="rating rating-xs">
+                  <input
+                    type="radio"
+                    name="rating-5"
+                    class="mask mask-star-2 bg-orange-400"
+                    aria-label="1 star"
+                  />
+                </div>
+                {electro.rating.rate}
+              </h1>
+              <h1>Price: ${electro.price}</h1>
+              <h1>Cate: {electro.category}</h1>
+              <h2
+                className="items-center cursor-pointer"
+                onClick={() => handleRemoveElectronic(electro.id)}
+              >
+                X
+              </h2>
+            </div>
+          ))}
         </div>
-        <div className="grid grid-cols-3 col-span-3">
+        {/* 3 Column Layout */}
+        <div className=" bg-[#ddd] p-5 rounded-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-1 gap-5  ">
           {fetchPromiseUse.map((electronicItem) => (
             <Electronic
+              handleGetElectronic={handleGetElectronic}
               key={electronicItem.id}
               electronicItem={electronicItem}
             ></Electronic>
